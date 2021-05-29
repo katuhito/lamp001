028ee71faa4143e189657d54ee1130b7b4554417
Katuhito Hara <katuhitohara@gmail.com>
2021-05-29 18:43:28 +0900

    increment, float, arithmetic

---
+++ float2.php
@@ -0,0 +1,7 @@
+
+<?php 
+$add = bcadd(0.1, 0.7, 1);
+$mul = bcmul($add, 10, 1);
+
+print floor($mul);  //結果：8
+
---
+++ m_increment.php
@@ -0,0 +1,9 @@
+<?php
+
+$i = 'z';
+print ++$i;
+print ++$i;
+
+$j = 'T8';
+print ++$j;
+print ++$j;
